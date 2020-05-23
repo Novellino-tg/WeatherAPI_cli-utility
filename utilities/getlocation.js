6a2f065e8de7f6e79165bb66264094af21d479a4
Tushar goyal <63923889+Novellino-tg@users.noreply.github.com>
2020-05-23 10:32:05 +0530

    Add files via upload

--- utilities/getlocation.js
+++ utilities/getlocation.js
@@ -1,1 +1,12 @@
-//
+const axios=require("axios");
+module.exports= async function (){
+    try{
+        const location= await axios.get("http://ip-api.com/json/");
+        return location.data.city;
+    }
+    catch(err){
+        console.log(err);
+    }
+    
+// console.log(location.data.city);
+}
